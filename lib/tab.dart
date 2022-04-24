38a9dc21670d889c9388ed68f3718268bc87ca97
Akhmad Khanif Zyen <khanif.zyen@unisnu.ac.id>
2022-04-24 23:24:26 +0700

    tab done

--- lib/tab.dart
+++ lib/tab.dart
@@ -11,12 +11,29 @@ class MyTab extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Tab Demo"),
           bottom: const TabBar(
+            //indicatorSize: TabBarIndicatorSize.label,
+            //indicatorColor: Colors.red,
+            indicatorWeight: 5,
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.white30,
+            indicator: BoxDecoration(
+              color: Colors.orangeAccent,
+              borderRadius: BorderRadius.all(Radius.circular(50)),
+            ),
+            //isScrollable: true,
             tabs: [
               Tab(icon: Icon(Icons.directions_car)),
               Tab(icon: Icon(Icons.directions_transit)),
               Tab(icon: Icon(Icons.directions_bike)),
             ],
           ),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text("Ini adalah mobil")),
+            Center(child: Text("Ini adalah transit")),
+            Center(child: Text("Ini adalah sepeda")),
+          ],
         ),
       ),
     );
